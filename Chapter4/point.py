950877551c245d8350db222224c4604d82bb9134
David Gugea <davidgugea@yahoo.com>
2022-06-26 09:44:22 +0200

    Finished Chapter 4

---
+++ Chapter4/point.py
@@ -0,0 +1,9 @@
+class Point():
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
