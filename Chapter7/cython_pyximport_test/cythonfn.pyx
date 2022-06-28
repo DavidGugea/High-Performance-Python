1ba5747d8d63d825ec7f1250635bd60ffadbf2c1
David Gugea <davidgugea@yahoo.com>
2022-06-28 20:01:12 +0200

    Working on Chapter 7

---
+++ Chapter7/cython_pyximport_test/cythonfn.pyx
@@ -0,0 +1,17 @@
+# cythonfn.pyx
+
+def calculate_z(maxiter, zs, cs):
+    """Calcualte output list using Julia update rule"""
+    output = [0] * len(zs)
+    for i in range(len(zs)):
+        n = 0
+        z = zs[i]
+        c = cs[i]
+
+        while n < maxiter and abs(z) < 2:
+            z = z * z + c
+            n += 1
+
+        output[i] = n
+
+    return output
